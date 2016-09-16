4a7ad132bb2105ffca57d9134b5e3106764c494a
Debapriya Das <yodebu@gmail.com>
2016-09-16 20:46:22 +0530

    /questions/ + /apti ,  /code ,  /psycho , /tech

--- routes/questions.js
+++ routes/questions.js
@@ -163,12 +163,12 @@ router.route('/tech')
 
 
 
-// APTI Questions
-//build the REST operations at the path = /questions/apti
-//this will be accessible from http://127.0.0.1:3000/questions/apti
+// Psychometric Questions
+//build the REST operations at the path = /questions/psycho
+//this will be accessible from http://127.0.0.1:3000/questions/psycho
 
 router.route('/psycho')
-//GET all apti questions
+//GET all psychometric questions
     .get(function(req, res, next) {
         //retrieve all blobs from Monogo
         mongoose.model('Psycho').find({}, function (err, question) {
@@ -209,7 +209,6 @@ router.route('/psycho')
                 console.log('POST creating new Psycho: ' + question);
                 res.format({
 
-
                     //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
                     // html: function(){
                     //     // If it worked, set the header so the address bar doesn't still say /adduser
@@ -229,6 +228,69 @@ router.route('/psycho')
 
 
 
+
+
+
+// CODING Questions
+//build the REST operations at the path = /questions/code
+//this will be accessible from http://127.0.0.1:3000/questions/code
+
+router.route('/code')
+//GET all psychometric questions
+    .get(function(req, res, next) {
+        //retrieve all blobs from Monogo
+        mongoose.model('Code').find({}, function (err, question) {
+            if (err) {
+                return console.error(err);
+            } else {
+                //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                res.format({
+                    //JSON response will show all blobs in JSON format
+                    json: function(){
+                        res.json(question);
+                    }
+                });
+            }
+        });
+    })
+
+    .post(function(req, res) {
+        // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
+        var testID = req.body.testID;
+        var ques = req.body.ques;
+        var input =  req.body.input;
+        var output = req.body.output;
+
+        //call the create function for our database
+        mongoose.model('Code').create({
+            testID: testID,
+            ques: ques,
+            input: input,
+            output: output
+        }, function (err, question) {
+            if (err) {
+                res.send("There was a problem adding the information to the database.");
+            } else {
+                //Blob for CODE has been created
+                console.log('POST creating new Code: ' + question);
+                res.format({
+
+                    //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
+                    // html: function(){
+                    //     // If it worked, set the header so the address bar doesn't still say /adduser
+                    //     res.location("blobs");
+                    //     // And forward to success page
+                    //     res.redirect("/blobs");
+                    // },
+
+                    //JSON response will show the newly created blob
+                    json: function(){
+                        res.json(question);
+                    }
+                });
+            }
+        })
+    });
 
 
 
